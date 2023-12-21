005c6c367ffa089e5871df80e18068eeaf6e2db3
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-21 06:10:12 +0300

    component folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
     <>
-     hellooo
+    <Header/>
+    <h1>Hello</h1>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      header anaam
+    </div>
+  )
+}
+
+export default Header
