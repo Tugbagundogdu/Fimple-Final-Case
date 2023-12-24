57a6f12650927d61c70b52ea15a195dc9dbfb997
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-24 05:27:08 +0300

    route paths added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
-
+import './App.css';
+import Router from "./components/Router/Router";
 function App() {
+
 
   return (
     <>
     <Header/>
-    <h1>Hello</h1>
+    <Router/>
     <Footer/>
     </>
   )
---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,30 @@
+
+import {  Routes, Route } from 'react-router-dom';
+import ApplicationForm from "../ApplicationForm/ApplicationForm";
+import AdminPanel from "../AdminPanel/AdminPanel";
+import Homepage from '../Homepage/Homepage';
+import ApplicationSuccess from '../ApplicationSuccess/ApplicationSuccess';
+import ApplicationInquiry from '../ApplicationInquiry/ApplicationInquiry';
+import ApplicationDetail from '../ApplicationDetail/ApplicationDetail';
+import AdminApplyListDetail from '../AdminApplyListDetail/AdminApplyListDetail';
+import AdminApplyList from '../AdminApplyList/AdminApplyList';
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/basvuru-olustur" element={<ApplicationForm/>} />
+        <Route path="/basvuru-basarili" element={<ApplicationSuccess/>}/>
+        <Route path="/basvuru-sorgula" element={<ApplicationInquiry/>}/>
+        <Route path="/basvuru/:basvuruNo" element={<ApplicationDetail/>}/>
+        <Route path="/admin" element={<AdminPanel/>} />
+        <Route path="/admin/basvuru-listesi" element={<AdminApplyList/>}/>
+        <Route path="/admin/basvuru/:basvuruNo" element={<AdminApplyListDetail/>}/>
+    
+        {/* Burada NotFound sayfasını ekleyebilirsiniz */}
+        {/* <Route component={NotFound} /> */}
+    </Routes>
+  );
+};
+
+export default Router;
