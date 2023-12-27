d3b8ea474e3c4bfe4f80e23d811b10e5315facf4
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-28 01:43:48 +0300

    firebase files fixed

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBsSB50GgEhpIvqsIgET-Uy3vX-QPU-wP8",
+  authDomain: "fimple-final.firebaseapp.com",
+  projectId: "fimple-final",
+  storageBucket: "fimple-final.appspot.com",
+  messagingSenderId: "667823951623",
+  appId: "1:667823951623:web:fe4b74004dd5ef3142bfa2",
+  measurementId: "G-WK9JHD1WJG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
