437e20384bcbc3b0d0f93198b3db415a24be7e8d
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-28 01:43:59 +0300

    provider added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Header from "./components/Header/Header"
 import './App.css';
 import Router from "./components/Router/Router";
 import FormDataProvider from "./context/FormDataProvider";
+import ApplicationResultProvider from "./context/ApplicationResult";
 function App() {
 
 
   return (
     <>
+    <ApplicationResultProvider>
     <FormDataProvider>
     <Header/>
     <Router/>
     </FormDataProvider>
-
+    </ApplicationResultProvider>
     </>
   )
 }
