45e3658921a2da3c81755536c910a6f91ba00eae
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-30 05:36:35 +0300

    admin login auth added

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,16 +1,41 @@
-import { useNavigate } from "react-router-dom"
-const AdminPanel = () => {
-    const navigate = useNavigate();
+import { useForm } from "react-hook-form";
+import "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const AdminLogin = () => {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const onSubmit = async (data) => {
+    const { email, password } = data;
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log("Giriş başarılı, kullanıcı:", user);
+      navigate("/admin/basvuru-listesi");
+      // Başarılı giriş durumunda yönlendirme veya diğer işlemleri yapabilirsiniz
+    } catch (error) {
+      // Giriş hatası durumunda
+      console.log("Giriş hatası:", error.message);
+    }
+  };
+
   return (
     <div>
-        <h2>ADMİN PANEL</h2>
-      <form action="">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-        <button onClick={() => navigate("/admin/basvuru-listesi")} type="submit">Submit</button>
+      <h2>Admin Girişi</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input type="email" {...register("email")} id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" {...register("password")} id="password" />
+        <button type="submit">Giriş</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPanel
+export default AdminLogin;
