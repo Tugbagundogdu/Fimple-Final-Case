f17a3cdab7c274d0a07da00f463da59d87f3b47b
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2024-01-01 06:18:09 +0300

    form schema fixed

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -5,7 +5,7 @@ export  const formSchema = yup.object().shape({
     surname: yup.string().required('Surname is required'),
     email: yup.string().email('Email format is not valid').required('Email is required'),
     age: yup.number().positive().integer().typeError('Age must be a number').max(40).min(18). required('Age is required'),
-    tc: yup.number().required('TC is required').matches(/^[0-9]{11}$/, "TC must be 11 digits"),
+    tc: yup.string().required('TC is required').matches(/^[0-9]{11}$/, "TC must be 11 digits"),
     application: yup.string().required('Application is required'),
     address: yup.string().required('Address is required'),
     
